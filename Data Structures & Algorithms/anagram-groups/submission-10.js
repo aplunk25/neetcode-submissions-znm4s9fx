7329d5ee67cb5e560f17a15daaf8687264776594
Alex Plunk <a.plunk25@gmail.com>
2026-09-17 22:46:41 -0500

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {};
+
+        for(let str of strs){
+            let sorted = str.split('').sort().join('');
+            if(!res[sorted]){
+                res[sorted] = [];
+            }
+            res[sorted].push(str);
+        }
+        return Object.values(res);
+    }
+}
